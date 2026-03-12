77a534f4736406e664a049a6ebae11d11ec2a94b
harishpalepu222 <harishpalepu222@gmail.com>
2026-03-12 13:02:16 +0530

    first commit

---
+++ src/test/java/com/example/__docker_jenkins_integration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__docker_jenkins_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
